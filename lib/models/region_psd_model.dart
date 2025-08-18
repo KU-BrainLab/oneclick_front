db4c47e4452eeb5f637e26792ed6bed63d092605
shikgom2 <shikgom2@naver.com>
2025-08-18 15:48:02 +0900

    fix : update calculate percentage

--- lib/models/region_psd_model.dart
+++ lib/models/region_psd_model.dart
@@ -12,31 +12,26 @@ class RegionPsdModel {
   });
 
   factory RegionPsdModel.fromJson(List<dynamic> leftList, List<dynamic> rightList) {
-    List<double> left = [];
-    List<double> right = [];
-    double max = 0;
+    List<double> leftRaw = leftList.map((e) => (e as num).toDouble()).toList();
+    List<double> rightRaw = rightList.map((e) => (e as num).toDouble()).toList();
 
-    leftList.forEach((element) {
-      
-      double ele = (element as double) * 100;
-      
-      left.add(ele);
-      if(max < ele) {
-        max = ele;
-      }
-    });
+    double leftSum = leftRaw.fold(0.0, (a,b) => a+b);
+    double rightSum = rightRaw.fold(0.0, (a,b) => a+b);
 
-    rightList.forEach((element) {
-      double ele = (element as double) * 100;
+    List<double> left = leftSum == 0
+        ? List<double>.filled(leftRaw.length, 0.0)
+        : leftRaw.map((v) => (v / leftSum) * 100.0).toList();
 
-      right.add(ele);
-      if(max < ele) {
-        max = ele;
-      }
-    });
+    List<double> right = rightSum == 0
+        ? List<double>.filled(rightRaw.length, 0.0)
+        : rightRaw.map((v) => (v / rightSum) * 100.0).toList();
+
+    double max = 0.0;
+    for (final v in left)  { if (v > max) max = v; }
+    for (final v in right) { if (v > max) max = v; }
 
     double interval = 5;
-
+    
     return RegionPsdModel(left: left, right: right, max: max, interval: interval);
   }
 
