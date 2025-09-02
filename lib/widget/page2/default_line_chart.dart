6f0d21bfca25724e5fe54f89be87976dc6c906ec
shikgom2 <shikgom2@naver.com>
2025-09-02 16:28:48 +0900

    feat: update Raw PSD chart
    Raw PSD chart  비주얼 업데이트 (추후 살펴보고 롤백 및 수정여부 결정)

--- lib/widget/page2/default_line_chart.dart
+++ lib/widget/page2/default_line_chart.dart
@@ -18,13 +18,21 @@ class DefaultLineChart extends StatelessWidget {
   Widget build(BuildContext context) {
     return _buildDefaultLineChart();
   }
-
   /// Get the cartesian chart with default line series
   Container _buildDefaultLineChart() {
     double y0 = min(0.0, model.minY);
     if(y0 == -5){
       y0 = 0;
     }
+
+    final List<Color> fillColors = [
+      const Color(0xFFBEE9E8).withOpacity(0.7),
+      const Color(0xFF62B6CB).withOpacity(0.7),
+      const Color(0xFF1B4965).withOpacity(0.7),
+      const Color(0xFFCAE9FF).withOpacity(0.7),
+      const Color(0xFF5FA8D3).withOpacity(0.7),
+    ];
+
     return Container(
       decoration: BoxDecoration(
         border: Border.all(color: Colors.black)
@@ -39,6 +47,14 @@ class DefaultLineChart extends StatelessWidget {
             labelIntersectAction: AxisLabelIntersectAction.none,
             majorGridLines: const MajorGridLines(width: 1),
             title: const AxisTitle(text: "Frequency (Hz)"),
+            
+            plotBands: <PlotBand>[
+              PlotBand(start: 0, end: 4, color: fillColors[0]),
+              PlotBand(start: 4, end: 8, color: fillColors[1]),
+              PlotBand(start: 8, end: 12, color: fillColors[2]),
+              PlotBand(start: 12, end: 30, color: fillColors[3]),
+              PlotBand(start: 30, end: 45, color: fillColors[4]),
+            ],
           ),
           primaryYAxis: NumericAxis(
             minimum: y0,
@@ -51,12 +67,49 @@ class DefaultLineChart extends StatelessWidget {
             },
             title: const AxisTitle(text: "10*log10(Power) + C"),
           ),
+
+          annotations: <CartesianChartAnnotation>[
+            CartesianChartAnnotation(
+              widget: const Text('δ', style: TextStyle(color: Colors.black54, fontWeight: FontWeight.w500)),
+              coordinateUnit: CoordinateUnit.point,
+              x: 2,
+              y: model.maxY * 0.95,
+            ),
+            CartesianChartAnnotation(
+              widget: const Text('θ', style: TextStyle(color: Colors.black54, fontWeight: FontWeight.w500)),
+              coordinateUnit: CoordinateUnit.point,
+              x: 6, 
+              y: model.maxY * 0.95,
+            ),
+            CartesianChartAnnotation(
+              widget: const Text('α', style: TextStyle(color: Colors.black54, fontWeight: FontWeight.w500)),
+              coordinateUnit: CoordinateUnit.point,
+              x: 10,
+              y: model.maxY * 0.95,
+            ),
+            CartesianChartAnnotation(
+              widget: const Text('β', style: TextStyle(color: Colors.black54, fontWeight: FontWeight.w500)),
+              coordinateUnit: CoordinateUnit.point,
+              x: 21,
+              y: model.maxY * 0.95,
+            ),
+            CartesianChartAnnotation(
+              widget: const Text('γ', style: TextStyle(color: Colors.black54, fontWeight: FontWeight.w500)),
+              coordinateUnit: CoordinateUnit.point,
+              x: 35,
+              y: model.maxY * 0.95,
+            ),
+          ],
+
+
           series: <CartesianSeries>[
             FastLineSeries<FlSpot, double>(
                 dataSource: model.dataList,
                 xValueMapper: (FlSpot data, _) => data.x,
                 yValueMapper: (FlSpot data, _) => data.y,
                 animationDuration: 0,
+                color: const Color(0xFF264653),
+                opacity: 0.6,
             ),
           ],
       ),
